b728a8723e75962477dc3bc07ee3bf911e082d52
3351812 <3351812@gmail.com>
2026-01-31 17:19:00 +0800

    fix(router): use hash history for github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,34 @@
-import { createRouter, createWebHistory } from "vue-router";
+// import { createRouter, createWebHistory } from "vue-router";
+// import routes from "./routes.js";
+// import { useAuthStore } from "../store/auth.store.js";
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+//   scrollBehavior() {
+//     return { left: 0, top: 0 };
+//   }
+// });
+
+// router.beforeEach((to) => {
+//   const auth = useAuthStore();
+//   const isAuthRoute = to.path.startsWith("/auth");
+//   if (!isAuthRoute && !auth.isAuthed) {
+//     return "/auth/login";
+//   }
+//   if (isAuthRoute && auth.isAuthed) {
+//     return "/app/command";
+//   }
+// });
+
+// export default router;
+
+import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes.js";
 import { useAuthStore } from "../store/auth.store.js";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(), // ✅ GitHub Pages 需要 hash
   routes,
   scrollBehavior() {
     return { left: 0, top: 0 };
